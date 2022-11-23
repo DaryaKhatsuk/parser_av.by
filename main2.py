16ee5997ac02f59bbf81c42d9802a135670d3c59
DaryaKhatsuk <112404482+DaryaKhatsuk@users.noreply.github.com>
2022-11-23 23:35:08 +0300

    commit

--- main2.py
+++ main2.py
@@ -1,10 +1,11 @@
 import csv
 import json
 import time
-
-import bs4
+import lxml
+from bs4 import BeautifulSoup
 import requests
 import asyncio
+import aiohttp
 
 JSON = 'cars.json'
 CSV = 'cars.csv'
@@ -16,13 +17,13 @@ HEADERS = {
 }
 
 
-def get_html(url, params=''):
-    req = requests.get(url, headers=HEADERS, params=params)
-    return req
+# async def get_html(url, params=''):
+#     req = await requests.get(url, headers=HEADERS, params=params)
+#     return req
 
 
-def get_content(html):
-    soup = bs4.BeautifulSoup(html, 'html.parser')
+async def get_content(html):
+    soup = BeautifulSoup(await html, 'html.parser')
     items = soup.find_all('div', class_='listing-item__wrap')
     cards = []
     for item in items:
@@ -39,20 +40,25 @@ def get_content(html):
                     'price_car_usd': price_car_usd, 'message_car': message_car}
             cards.append(card)
         except Exception as ex:
-            print(f'Some {ex} here')
+            title_car = item.find('span', class_='link-text').text
+            href_car = ['https://moto.av.by' + item.find('a', class_='listing-item__link').get('href')]
+            price_car_byn = item.find('div', class_='listing-item__price').text.replace('\xa0', ' ').replace(
+                '\u2009', ' ')
+            price_car_usd = item.find('div', class_='listing-item__priceusd').text.replace('\xa0', ' ').replace(
+                '\u2009', ' ')
+            card = {'title_car': title_car, 'href_car': href_car, 'price_car_byn': price_car_byn,
+                    'price_car_usd': price_car_usd, 'message_car': 'Pass'}
+            cards.append(card)
+            print(f'Some {ex} here.')
 
-    return cards
+    safe_doc(cards)
 
 
-def safe_doc(items, path):
-    with open(path, 'w', newline='', encoding='UTF-8') as file:
+def safe_doc(items):
+    with open(JSON, 'w', newline='', encoding='UTF-8') as file:
         sl = {}
         sc = 1
-
         for item in items:
-            with open('cars.csv', 'a+', newline='', encoding='UTF=8') as csvfile:
-                csvF = csv.writer(csvfile, delimiter=',', quotechar=' ', quoting=csv.QUOTE_ALL)
-                csvF.writerow(item.values())
             sl.update(
                 {
                     "title_car " + str(sc): item["title_car"],
@@ -64,25 +70,31 @@ def safe_doc(items, path):
             )
             sc += 1
         json.dump(sl, file, indent=4, ensure_ascii=False)
+    with open(CSV, 'a+', newline='', encoding='UTF=8') as csvfile:
+        for item in items:
+            csvF = csv.writer(csvfile, delimiter=',', quotechar=' ', quoting=csv.QUOTE_ALL)
+            csvF.writerow(item.values())
 
 
-def parser():
-    html = get_html(URL)
-    if html.status_code == 200:
-        counter = 1
-        while True:
-            html = get_html(URL, params='&page=' + str(counter))
-            print(f'Parsing page {counter}')
-            if html.status_code == 200:
-                get_content(html.text)
-                counter += 1
-            else:
-                break
-    else:
-        print("Error")
+async def parser():
+    async with aiohttp.ClientSession() as session:
+        html = await session.get(url=URL, headers=HEADERS)
+        if html.status == 200:
+            counter = 119
+            while True:
+                html = await session.get(url=URL + '&page=' + str(counter))
+                print(f'Parsing page {counter}')
+                if html.status == 200:
+                    asyncio.create_task(get_content(html.text()))
+                    counter += 1
+                else:
+                    break
+        else:
+            print("Error")
 
 
 x = time.time()
-parser()
+loop = asyncio.get_event_loop()
+loop.run_until_complete(asyncio.wait([parser()]))
 y = time.time()
 print(y-x)
